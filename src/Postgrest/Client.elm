e608d7d3583d170c92ec1fa685494f80ecef4ea8
Alex Tan <alex.r.tan@protonmail.com>
2020-04-14 12:57:11 +0800

    Array type

--- src/Postgrest/Client.elm
+++ src/Postgrest/Client.elm
@@ -71,6 +71,7 @@ module Postgrest.Client exposing
     , plfts
     , phfts
     , fts
+    , array
     )
 
 {-|
@@ -739,8 +740,15 @@ resourceWithParams =
 `inList`.
 -}
 list : List Value -> Value
-list values =
-    List values
+list =
+    List
+
+
+{-| Represent an array.
+-}
+array : List Value -> Value
+array =
+    Array
 
 
 {-| Shorthand for attributes, when you don't need to specify nested resources:
